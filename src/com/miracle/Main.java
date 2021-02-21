eb730878ac007c1c160ba930f3e3efe66804796e
liuxinjie123 <jackdream@163.com>
2021-02-21 22:53:17 +0800

    java algorithm

--- src/com/miracle/Main.java
+++ src/com/miracle/Main.java
@@ -4,41 +4,21 @@ import java.util.Scanner;
 
 public final class Main {	
 	public static void main(String[] args) {
-		char[] buf = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-
 		Scanner scanner = new Scanner(System.in);
 		String str = scanner.next();
-		
-		int count = 0;
-		int num = 0;
-		for (int i=0; i<str.length();) {
-			if (str.charAt(i) == buf[num]) {
-				num++;
-				i++;
-				boolean success = false;
-				while (i<str.length() && num <= 25) {
-					if (str.charAt(i) == buf[num]) {
-						i++;
-						num++;
-						if (buf[num] == 'z') {
-							success = true;
-							break;
-						}
-					} else {
-						num++;
-						if (num > 25) {
-							break;
-						}
-						count++;
-					}					
+		int[] dp = new int[str.length()];
+		dp[0] = 1;
+		int ans = 1;
+		for (int i=1; i<str.length(); i++) {
+			dp[i] = 1;
+			for (int j=0; j<i; j++) {
+				if (str.charAt(i) > str.charAt(j)) {
+					dp[i] = Math.max(dp[i], dp[j]+1);
 				}
-				if (!success) {
-					if (str.charAt(str.length()-1) != 'z') {
-						count += (int)buf[25] - (int)str.charAt(str.length()) - 1;
-					}
-				}
-			} 
+			}
+			ans = Math.max(ans, dp[i]);
 		}
-		System.out.println(count);
+		System.out.println(26 - ans);
+		return;
 	}	
 }
