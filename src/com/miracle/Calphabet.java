273a266b7c0cfb93f46161d038157cb34c9ee0ff
liuxinjie123 <jackdream@163.com>
2021-02-21 22:53:52 +0800

    java algorithm

--- src/com/miracle/Calphabet.java
+++ src/com/miracle/Calphabet.java
@@ -1,5 +1,24 @@
 package com.miracle;
 
-public class Calphabet {
+import java.util.Scanner;
 
+public class Calphabet {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        int[] dp = new int[str.length()];
+        dp[0] = 1;
+        int ans = 1;
+        for (int i=1; i<str.length(); i++) {
+            dp[i] = 1;
+            for (int j=0; j<i; j++) {
+                if (str.charAt(i) > str.charAt(j)) {
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+            ans = Math.max(ans, dp[i]);
+        }
+        System.out.println(26 - ans);
+        return;
+    }
 }
