56f7ecbda4b94c3a64d08108c1ca0084bc2d4971
1134005157 <1134005157@qq.com>
2021-02-23 15:44:33 +0800

    algorithm

--- src/com/miracle/FillNineForm.java
+++ src/com/miracle/FillNineForm.java
@@ -8,17 +8,6 @@ import java.util.Set;
 public class FillNineForm {
     static int size = 6;
     static Node[][] buf = new Node[size][size];
-    static Set<Integer> valSet = new HashSet<Integer>(){{
-        add(1);
-        add(2);
-        add(3);
-        add(4);
-        add(5);
-        add(6);
-        add(7);
-        add(8);
-        add(9);
-    }};
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -31,8 +20,8 @@ public class FillNineForm {
                 if (str.contains("/")) {
                     String[] nums = str.split("/");
                     node.type = 2;
-                    node.left = nums[0].equals("-") ? 0: Integer.valueOf(nums[0]).intValue();
-                    node.right = nums[1].equals("-") ? 0: Integer.valueOf(nums[1]).intValue();
+                    node.left = nums[0].equals("-") ? 0: Integer.valueOf(nums[0]);
+                    node.right = nums[1].equals("-") ? 0: Integer.valueOf(nums[1]);
                     buf[i][j] = node;
                 } else {
                     node.type = 1;
@@ -42,17 +31,39 @@ public class FillNineForm {
             }
         }
 
+        System.out.println();
+        for (int i=0; i<size; i++) {
+            for (int j=0; j<size; j++) {
+                Node node = buf[i][j];
+                if (j != size - 1) {
+                    if (node.type == 1) {
+                        System.out.print(node.value + " ");
+                    } else {
+                        System.out.print(node.left + "/" + node.right + " ");
+                    }
+                } else {
+                    if (node.type == 1) {
+                        System.out.print(node.value);
+                    } else {
+                        System.out.print(node.left + "/" + node.right);
+                    }
+                }
+            }
+            System.out.println();
+        }
+
         int count = 0;
         while (count < 36) {
             count = 0;
             for (int i=0; i<size; i++) {
                 for (int j=0; j<size; j++) {
-                    Node node = buf[i][j];
-                    if (node.isNotEmpty()) {
+                    if (buf[i][j].isNotEmpty()) {
                         count++;
                     } else {
-                        node = findRightValue(node);
-                        buf[i][j] = node;
+                        buf[i][j] = findRightValue(buf[i][j]);
+                        if (buf[i][j].isNotEmpty()) {
+                            count++;
+                        }
                     }
                 }
             }
@@ -79,7 +90,6 @@ public class FillNineForm {
             }
         }
 
-
         System.out.println();
         for (int i=0; i<size; i++) {
             for (int j=0; j<size; j++) {
@@ -100,7 +110,6 @@ public class FillNineForm {
             }
             System.out.println();
         }
-
         return;
     }
 
@@ -115,13 +124,9 @@ public class FillNineForm {
         boolean isNotEmpty() {
             return (type==1 && value != 0) || (type==2 && left != 0 && right != 0);
         }
-
     }
 
     static Node findRightValue(Node node) {
-        if (node.isNotEmpty()) {
-            return node;
-        }
         Set<Integer> temp = new HashSet<Integer>(){{
             add(1);
             add(2);
@@ -133,19 +138,10 @@ public class FillNineForm {
             add(8);
             add(9);
         }};
-        // 删除同行同列中已存在的数字
+        // 删除同行中已存在的数字
         for (int i=0; i<size; i++) {
             if (i == node.column) {
-                if (1 == node.type) {
-                    continue;
-                } else {
-                    if (0 != node.left) {
-                        temp.remove(node.left);
-                    }
-                    if (0 != node.right) {
-                        temp.remove(node.right);
-                    }
-                }
+                continue;
             }
             Node newNode = buf[node.row][i];
             if (newNode.isNotEmpty()) {
@@ -166,18 +162,10 @@ public class FillNineForm {
                 }
             }
         }
+        // 删除同列的元素
         for (int i=0; i<size; i++) {
             if (i == node.row) {
-                if (1 == node.type) {
-                    continue;
-                } else {
-                    if (0 != node.left) {
-                        temp.remove(node.left);
-                    }
-                    if (0 != node.right) {
-                        temp.remove(node.right);
-                    }
-                }
+                continue;
             }
             Node newNode = buf[i][node.column];
             if (newNode.isNotEmpty()) {
@@ -199,12 +187,37 @@ public class FillNineForm {
             }
         }
 
-        if (node.type == 1) {
-            if (temp.size() == 1) {
-                Iterator it = temp.iterator();
-                node.value = (int) it.next();
-                return node;
+        // 删除小方框中的元素
+        int left = node.row <= 1 ? 0 : (node.row <= 3 ? 2 : 4);
+        int right = node.column <= 2 ? 0 : 3;
+
+        for (int m=left; m<left+2; m++) {
+            for (int n=right; n<right+3; n++) {
+                Node newNode = buf[m][n];
+                if (newNode.isNotEmpty()) {
+                    if (1 == newNode.type) {
+                        temp.remove(newNode.value);
+                    } else {
+                        temp.remove(newNode.left);
+                        temp.remove(newNode.right);
+                    }
+                } else {
+                    if (2 == node.type) {
+                        if (0 != newNode.left) {
+                            temp.remove(newNode.left);
+                        }
+                        if (0 != newNode.right) {
+                            temp.remove(newNode.right);
+                        }
+                    }
+                }
             }
+        }
+
+        if (node.type == 1 && temp.size() == 1) {
+            Iterator it = temp.iterator();
+            node.value = (int) it.next();
+            return node;
         } else if (node.type == 2) {
             if (temp.size() == 1) {
                 Iterator it = temp.iterator();
@@ -228,16 +241,12 @@ public class FillNineForm {
                     node.right = large;
                     return node;
                 } else {
-                    if (0 == node.left) {
-                        if (small < node.right && large > node.right) {
-                            node.left = small;
-                            return node;
-                        }
-                    } else if (0 == node.right) {
-                        if (small < node.left && large > node.left) {
-                            node.right = large;
-                            return node;
-                        }
+                    if (0 == node.left && small < node.right && large > node.right) {
+                        node.left = small;
+                        return node;
+                    } else if (0 == node.right && small < node.left && large > node.left) {
+                        node.right = large;
+                        return node;
                     }
                 }
             } else {
@@ -259,8 +268,7 @@ public class FillNineForm {
                 if (0 == node.left && 0 != node.right && smallTimes == 1) {
                     node.left = small;
                     return node;
-                }
-                if (0 != node.left && 0 == node.right && largeTimes == 1) {
+                } else if (0 != node.left && 0 == node.right && largeTimes == 1) {
                     node.right = large;
                     return node;
                 }
--- src/com/miracle/Main.java
+++ src/com/miracle/Main.java
@@ -5,13 +5,12 @@ import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
 
-public final class Main {
+public class Main {
 	static int size = 6;
 	static Node[][] buf = new Node[size][size];
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		String content = scanner.next();
 		for (int i=0; i<size; i++) {
 			for (int j=0; j<size; j++) {
 				Node node = new Node();
@@ -32,17 +31,17 @@ public final class Main {
 			}
 		}
 
-		int count = 26;
-		while (count > 0) {
+		int count = 0;
+		while (count < 36) {
+			count = 0;
 			for (int i=0; i<size; i++) {
 				for (int j=0; j<size; j++) {
 					Node node = buf[i][j];
-					if (!node.isNotEmpty()) {
+					if (node.isNotEmpty()) {
+						count++;
+					} else {
 						node = findRightValue(node);
 						buf[i][j] = node;
-						if (node.isNotEmpty()) {
-							count--;
-						}
 					}
 				}
 			}
@@ -69,7 +68,6 @@ public final class Main {
 			}
 			System.out.println();
 		}
-
 		return;
 	}
 
@@ -84,11 +82,10 @@ public final class Main {
 		boolean isNotEmpty() {
 			return (type==1 && value != 0) || (type==2 && left != 0 && right != 0);
 		}
-
 	}
 
 	static Node findRightValue(Node node) {
-		Set<Integer> valSet = new HashSet<Integer>(){{
+		Set<Integer> temp = new HashSet<Integer>(){{
 			add(1);
 			add(2);
 			add(3);
@@ -99,50 +96,91 @@ public final class Main {
 			add(8);
 			add(9);
 		}};
-		if (node.isNotEmpty()) {
-			return node;
-		}
-		Set<Integer> temp = valSet;
+		// 删除同行中已存在的数字
 		for (int i=0; i<size; i++) {
 			if (i == node.column) {
 				continue;
 			}
-			if (buf[node.row][i].isNotEmpty()) {
-				if (1 == node.type) {
-					temp.remove(node.value);
+			Node newNode = buf[node.row][i];
+			if (newNode.isNotEmpty()) {
+				if (1 == newNode.type) {
+					temp.remove(newNode.value);
 				} else {
-					temp.remove(node.left);
-					temp.remove(node.right);
+					temp.remove(newNode.left);
+					temp.remove(newNode.right);
 				}
 			} else {
-				if (0 != node.left) {
-					temp.remove(node.left);
-				}
-				if (0 != node.right) {
-					temp.remove(node.right);
+				if (2 == newNode.type) {
+					if (0 != newNode.left) {
+						temp.remove(newNode.left);
+					}
+					if (0 != newNode.right) {
+						temp.remove(newNode.right);
+					}
 				}
 			}
 		}
+		// 删除同列的元素
 		for (int i=0; i<size; i++) {
-			if (i == node.row) {
-				continue;
-			}
-			if (buf[i][node.column].isNotEmpty()) {
-				if (1 == node.type) {
-					temp.remove(node.value);
+			Node newNode = buf[i][node.column];
+			if (newNode.isNotEmpty()) {
+				if (1 == newNode.type) {
+					temp.remove(newNode.value);
 				} else {
-					temp.remove(node.left);
-					temp.remove(node.right);
+					temp.remove(newNode.left);
+					temp.remove(newNode.right);
 				}
 			} else {
-				if (0 != node.left) {
-					temp.remove(node.left);
-				}
-				if (0 != node.right) {
-					temp.remove(node.right);
+				if (2 == newNode.type) {
+					if (0 != newNode.left) {
+						temp.remove(newNode.left);
+					}
+					if (0 != newNode.right) {
+						temp.remove(newNode.right);
+					}
 				}
 			}
 		}
+
+		// 删除小方框中的元素
+		int left;
+		int right;
+		if (node.row <= 1) {
+			left = 0;
+		} else if (2 <= node.row && node.row <=3) {
+			left = 2;
+		} else {
+			left = 4;
+		}
+		if (node.column <= 2) {
+			right = 0;
+		} else {
+			right = 3;
+		}
+
+		for (int m=left; m<left+2; m++) {
+			for (int n=right; n<right+3; n++) {
+				Node newNode = buf[m][n];
+				if (newNode.isNotEmpty()) {
+					if (1 == newNode.type) {
+						temp.remove(newNode.value);
+					} else {
+						temp.remove(newNode.left);
+						temp.remove(newNode.right);
+					}
+				} else {
+					if (2 == node.type) {
+						if (0 != newNode.left) {
+							temp.remove(newNode.left);
+						}
+						if (0 != newNode.right) {
+							temp.remove(newNode.right);
+						}
+					}
+				}
+			}
+		}
+
 		if (node.type == 1) {
 			if (temp.size() == 1) {
 				Iterator it = temp.iterator();
@@ -150,12 +188,6 @@ public final class Main {
 				return node;
 			}
 		} else if (node.type == 2) {
-			if (0 != node.left) {
-				temp.remove(node.left);
-			}
-			if (0 != node.right) {
-				temp.remove(node.right);
-			}
 			if (temp.size() == 1) {
 				Iterator it = temp.iterator();
 				if (0 == node.left && 0 != node.right) {
@@ -181,13 +213,14 @@ public final class Main {
 					if (0 == node.left) {
 						if (small < node.right && large > node.right) {
 							node.left = small;
+							return node;
 						}
-					} else if (0 == node.left) {
+					} else if (0 == node.right) {
 						if (small < node.left && large > node.left) {
 							node.right = large;
+							return node;
 						}
 					}
-					return node;
 				}
 			} else {
 				int small = 0;
@@ -196,21 +229,22 @@ public final class Main {
 				int largeTimes = 0;
 
 				for (int item : temp) {
-					if (item > node.left) {
+					if (0 != node.left && item > node.left) {
 						large = item;
 						largeTimes++;
 					}
-					if (item < node.right) {
+					if (0 != node.right && item < node.right) {
 						small = item;
 						smallTimes++;
 					}
 				}
 				if (0 == node.left && 0 != node.right && smallTimes == 1) {
 					node.left = small;
+					return node;
 				} else if (0 != node.left && 0 == node.right && largeTimes == 1) {
 					node.right = large;
+					return node;
 				}
-				return node;
 			}
 		}
 		return node;
